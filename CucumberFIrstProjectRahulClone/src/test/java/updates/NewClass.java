a8d7af7a784402e7d0f7b0b53ea13ce208aa5912
TanushriMukherjee <tanushri.mukherjee@live.com>
2021-04-14 01:55:48 +0530

    Practice

---
+++ CucumberFIrstProjectRahulClone/src/test/java/updates/NewClass.java
@@ -0,0 +1,19 @@
+package updates;
+
+import org.testng.annotations.Test;
+
+public class NewClass {
+	
+	@Test
+	public void postJira() {
+		System.out.println("Postjira");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("Deletetwitter");
+	}
+
+}
+
+
